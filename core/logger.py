11caf10d86fee5fabe300bc60f5c83471c230df2
abysswan-cell <abysswan@gmail.com>
2026-08-06 22:59:07 -0700

    Add logging functionality to core/logger.py
    Implement a logger for KreaTrainer with INFO level.

--- core/logger.py
+++ core/logger.py
@@ -1,1 +1,27 @@
+import logging
+import sys
 
+LOG_FORMAT = "[%(levelname)s] %(message)s"
+
+
+def get_logger():
+
+    logger = logging.getLogger("KreaTrainer")
+
+    if logger.handlers:
+        return logger
+
+    logger.setLevel(logging.INFO)
+
+    handler = logging.StreamHandler(sys.stdout)
+
+    formatter = logging.Formatter(LOG_FORMAT)
+
+    handler.setFormatter(formatter)
+
+    logger.addHandler(handler)
+
+    return logger
+
+
+logger = get_logger()
